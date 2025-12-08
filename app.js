6ae2c9939b33c441e65c0bd081e39335fd0fc539
sami <adnansami@qq.com>
2025-12-08 19:30:38 +0800

    added done btn

--- app.js
+++ app.js
@@ -13,23 +13,28 @@ addBtn.addEventListener("click", () => {
             `<div class="taskCont">
                 <p class="task">${task}</p>
                 <div class="btnCont">
-                    <button id="editBtn" class="creBtn">Edit</button>
-                    <button id="doneBtn" class="creBtn">Done</button>
+                    <button class="creBtn editBtn">Edit</button>
+                    <button class="creBtn doneBtn">Done</button>
                 </div>
             </div>`
 
         inpBox.value = '';
     }
 
-    let doneBtn = document.querySelector("#doneBtn");
-    let taskText = document.querySelector(".task");
+    let doneBtn = document.querySelectorAll(".doneBtn");
 
+    doneBtn.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            let taskText = e.target.parentElement.previousElementSibling;
+            console.log(taskText);
 
-    doneBtn.addEventListener("click", () => {
-        console.log("done");
-        taskText.classList.add("remove");
-        console.log(taskText.innerText)
+            taskText.classList.add("remove");
+
+            console.log("done");
+        })
     })
+
+
 
 });
 
